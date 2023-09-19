6f943bd647cac7bb10ee9f263c5facaea65bdc9e
Gerardcoding <85459070+Gerardcoding@users.noreply.github.com>
2023-09-18 20:24:09 -0600

    Add files via upload

---
+++ TareaClase2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaClase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!!");
+            Console.ReadLine();
+
+        }
+    }
+}
